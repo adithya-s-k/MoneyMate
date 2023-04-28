95a1dd106312495c3567a801551bec91266d26ee
Adithya S K <adithyaskolavi@gmail.com>
2023-04-28 23:40:39 +0530

    added pricing options to testimonial - Adithya S K

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,39 @@
-import styles from "./style";
-import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
+import styles from './style';
+import {
+  Billing,
+  Business,
+  CardDeal,
+  Clients,
+  CTA,
+  Footer,
+  Navbar,
+  Stats,
+  Testimonials,
+  Hero,
+  // Pricing,
+} from './components';
 
 const App = () => (
-  <div className="bg-primary w-full overflow-hidden">
+  <div className="bg-primary1 w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Navbar />
       </div>
     </div>
 
-    <div className={`bg-primary ${styles.flexStart}`}>
+    <div className={`bg-primary1 ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
         <Hero />
       </div>
     </div>
-    
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+
+    <div className={`bg-primary1 ${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Stats />
         <Business />
         <Billing />
         <CardDeal />
+        {/* <Pricing /> */}
         <Testimonials />
         {/* <Clients /> */}
         <CTA />
